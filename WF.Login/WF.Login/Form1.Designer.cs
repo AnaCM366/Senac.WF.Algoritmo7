98fb7960dd3f23a89e8fd747436454747e4539b4
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-04-10 21:55:33 -0300

    Projeto de layout

--- WF.Login/WF.Login/Form1.Designer.cs
+++ WF.Login/WF.Login/Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(347, 477);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(46, 104);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(256, 235);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Black;
+            textBox1.Font = new Font("Rockwell", 10F);
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(463, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(453, 31);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Usuário";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Black;
+            textBox2.Font = new Font("Rockwell", 10F);
+            textBox2.ForeColor = Color.Gray;
+            textBox2.Location = new Point(463, 205);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(453, 31);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Coral;
+            button1.Font = new Font("Rockwell", 10F);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(463, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(453, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Cadastre-se";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 10F);
+            linkLabel1.Location = new Point(613, 372);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(172, 28);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueceu a senha?";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Image = Properties.Resources.menos_saida;
+            pictureBox2.Location = new Point(923, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(47, 29);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.White;
+            pictureBox3.Image = Properties.Resources.botao_saida;
+            pictureBox3.Location = new Point(986, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(47, 29);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(1045, 477);
+            Controls.Add(pictureBox3);
+            Controls.Add(linkLabel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.85D;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
     }
 }
